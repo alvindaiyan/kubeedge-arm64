1fa5643f730a69bf4fb47cbc6ecb7f660549b63f
Yan <alvindaiyan@gmail.com>
2020-03-12 12:30:29 +0800

    Update docker-install-pi.sh

--- docker-install-pi.sh
+++ docker-install-pi.sh
@@ -6,7 +6,7 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu bionic stable"
 
 sudo apt update
 
